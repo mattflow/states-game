d77ee205e8e1cc4080c29c0337fec849ca49b93e
Matt Flowers <mattmflowers28@gmail.com>
2019-02-20 20:20:13 -0500

    Add caret for collapse panel

--- src/components/CorrectInputs.js
+++ src/components/CorrectInputs.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import { Button, Collapse } from 'reactstrap';
+import { FaCaretDown, FaCaretUp } from 'react-icons/fa';
+
+const iconStyle = {
+  fontSize: 23,
+  marginRight: 5,
+  marginBottom: 4,
+};
 
 export default class CorrectInput extends React.Component {
   render() {
     return (
       <div>
-        <Button onClick={this.props.onShowToggle} color="link">Correct: {this.props.children.length}</Button>
+        <Button onClick={this.props.onShowToggle} color="link">
+          {this.props.open ? <FaCaretUp style={iconStyle} /> : <FaCaretDown style={iconStyle} />}
+          Correct: {this.props.children.length}
+        </Button>
         <Collapse isOpen={this.props.open}>
           {this.props.children}
         </Collapse>
