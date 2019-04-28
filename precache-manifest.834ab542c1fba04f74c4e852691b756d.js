d0fcb8d2dd4081ba3a056b92801eee343cac4100
Matt Flowers <mattmflowers28@gmail.com>
2019-04-28 16:28:59 -0400

    Updates

---
+++ precache-manifest.834ab542c1fba04f74c4e852691b756d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a46831d3cfe6ec62b00f",
+    "url": "/states-game/static/css/main.be9cd952.chunk.css"
+  },
+  {
+    "revision": "a46831d3cfe6ec62b00f",
+    "url": "/states-game/static/js/main.a46831d3.chunk.js"
+  },
+  {
+    "revision": "4d9430115eb803441eaa",
+    "url": "/states-game/static/js/runtime~main.4d943011.js"
+  },
+  {
+    "revision": "ed2526d7d2fb790e49c0",
+    "url": "/states-game/static/css/2.2e161b1e.chunk.css"
+  },
+  {
+    "revision": "ed2526d7d2fb790e49c0",
+    "url": "/states-game/static/js/2.ed2526d7.chunk.js"
+  },
+  {
+    "revision": "600a432defaf68994d98bb17d230e196",
+    "url": "/states-game/index.html"
+  }
+];
