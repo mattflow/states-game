b43523b23bd4186cf9ed6ea4b81cea0d00290694
Matt Flowers <mattmflowers28@gmail.com>
2022-11-10 15:18:31 -0500

    Chande deps and make button smaller

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,7 +8,7 @@ const Header = ({ resetOnClick }: HeaderProps) => {
   return (
     <div className="mb-4 flex items-center justify-between">
       <h1 className="text-4xl font-semibold">States Game</h1>
-      <button className="btn btn-primary" onClick={resetOnClick}>
+      <button className="btn-primary btn-sm btn" onClick={resetOnClick}>
         Reset
       </button>
     </div>
